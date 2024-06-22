24e700093f9ee6e15891fb29cdfcfcc10063bdb8
tamjidzihan <tamjidzihan@gmail.com>
2024-06-22 16:10:39 +0600

    Home 2024-06-22

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
 import useDatabase from './database/database';
 import ChatRoom from './components/ChatRoom';
 import SignIn from './components/SignIn';
 
 
-
-
 function App() {
   const { user } = useDatabase()
-
 
   return (
     <>
